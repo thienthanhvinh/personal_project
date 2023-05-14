73de8b3a12bc61dab607376d3b54acf023e0a0c2
Thanh Vinh <nguyenthienthanhvinh@gmail.com>
2023-05-14 17:40:21 +0700

    personal project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h2 class="text-right text-blue-500">Welcome to index</h2>
+</body>
+</html>
